44e1d2c3ab24a7a6f316352747cdb221d2eb21aa
Eddie <edwardchauke@hotmail.com>
2026-05-18 07:39:41 +0200

    Add dashboard URL

--- hospital/urls.py
+++ hospital/urls.py
@@ -5,6 +5,8 @@ from .views import fix_admin
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
 
+    path('dashboard/', views.dashboard, name='dashboard'),
+
     path('reception/', views.reception, name='reception'),
     path('appointments/', views.appointments, name='appointments'),
     path('prescriptions/', views.prescriptions, name='prescriptions'),
