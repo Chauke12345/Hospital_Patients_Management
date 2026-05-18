fe883b805ca0ef67c081568f048079c5dc016b35
Eddie <edwardchauke@hotmail.com>
2026-05-18 09:12:10 +0200

    update patient list

--- patients/urls.py
+++ patients/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", patient_list, name="patient_list"),
     path("add/", add_patient, name="add_patient"),
     path("discharge/<int:patient_id>/", discharge_patient, name="discharge_patient"),
+   
 ]
