0383ec8bff62d780b0a373a9daff068d16c4ded2
Eddie <edwardchauke@hotmail.com>
2026-05-25 11:19:15 +0200

    fix broken migration file structure

--- hospital/migrations/0002_admission_doctor_emergency_doctor.py
+++ hospital/migrations/0002_admission_doctor_emergency_doctor.py
@@ -1,1 +1,32 @@
-operations = []
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hospital', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='admission',
+            name='doctor',
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to='hospital.doctor'
+            ),
+        ),
+        migrations.AddField(
+            model_name='emergency',
+            name='doctor',
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to='hospital.doctor'
+            ),
+        ),
+    ]
